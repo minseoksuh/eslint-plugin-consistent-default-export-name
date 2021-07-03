1cc69c1f02d8b00e9b056448cd15285e236a20e1
서민석 <minseoksuh@mins-MacBook-Pro.local>
2021-07-04 03:47:46 +0900

    feat, refactor: write primitive rule, change filename

---
+++ tests/lib/rules/filename-default-export.js
@@ -0,0 +1,33 @@
+import rule from '../../../lib/rules/filename-default-export.js';
+import { RuleTester } from 'eslint';
+
+console.log({ rule });
+
+const ruleTester = new RuleTester();
+
+const exportedEs6VariableCode = 'export default exported;';
+
+ruleTester.run('test', rule, {
+    valid: [
+        {
+            code: exportedEs6VariableCode,
+            filename: '/some/dir/exported.js',
+            parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+        },
+    ],
+
+    invalid: [
+        {
+            code: exportedEs6VariableCode,
+            filename: '/some/dir/fooBar.js',
+            parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+            errors: [
+                {
+                    message: 'default export name and filename does not match',
+                    column: 1,
+                    line: 1,
+                },
+            ],
+        },
+    ],
+});
