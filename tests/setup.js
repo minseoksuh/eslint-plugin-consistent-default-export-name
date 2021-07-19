eb5ecc731bacef87811b223ca232cc9fe008abba
서민석 <minseoksuh@mins-MacBook-Pro.local>
2021-07-19 10:10:33 +0900

    fix: fix tests about index.js using sinon

---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+const sinon = require('sinon');
+const { beforeEach, afterEach } = require('mocha');
+
+beforeEach(function () {
+    sinon.stub(process, 'cwd').returns('/foo');
+});
+
+afterEach(function () {
+    process.cwd.restore();
+});
